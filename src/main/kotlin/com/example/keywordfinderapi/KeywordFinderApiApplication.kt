82242a9479f0a95897b5d1a3d8cc0dabf3d9103d
박준하 <wnsgkqkr@naver.com>
2021-05-05 21:35:13 +0900

    최초 프로젝트 생성 커밋

---
+++ src/main/kotlin/com/example/keywordfinderapi/KeywordFinderApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.keywordfinderapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KeywordFinderApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<KeywordFinderApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/keywordfinderapi/KeywordFinderApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.keywordfinderapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KeywordFinderApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
